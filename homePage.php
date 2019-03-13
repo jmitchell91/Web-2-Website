17eb5c6154bc8c2f161ba6312e998862048406c0
Chris Rand <crand4@radford.edu>
2019-03-13 15:19:56 -0400

    Sidebar
    Created sidebar.css and sidebar.php to render a sidebar on pages that include, also commented out adminLogin stylesheet for the homepage to get correct header rendering

--- homePage.php
+++ homePage.php
@@ -8,16 +8,17 @@
             Home Page
         </TITLE>
     </HEAD>
-    <link rel="stylesheet" href="adminLogin.css">
+    <!--<link rel="stylesheet" href="adminLogin.css">-->
     <script>
 
     </script>
+	<?php include ("sidebar.php");?>
     <BODY>
         <H1>
             Home Page
         </H1>
         <P>
-            This Page is the normal page for users who aren't logged in
-        </P>
+			Holds homepage content
+		</P>
     </BODY>
 </HTML>
