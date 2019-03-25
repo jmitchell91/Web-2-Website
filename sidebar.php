0128a9f7e5c893ec459cc524e017d2a0bac90199
Chris Rand <crand4@radford.edu>
2019-03-25 13:55:13 -0400

    revision of header/sidebar
    Revised header/sidebar generators to only include necessary html, and added stylesheetImport.php to import all required styleshets

--- sidebar.php
+++ sidebar.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html>
-	<head>
-        <link rel="stylesheet" href="sidebar.css">
-	</head>
-	<body>
-		<div class="sidenav">
-			Help Desk:
-			<ul>
-				<li><a href="">Bank Accounts</a></li>
-				<li><a href="">Claims</a></li>
-				<li><a href="">Hearings</a></li>
-				<li><a href="">Tips for Beneficiaries</a></li>
-				<li><a href="">Tips for Fiduciaries</a></li>
-				<li><a href="">Tips for Filing Your Account</a></li>
-			</ul>
-			<br />
-			Contact Information:
-		</div>
-	</body>
-</html>
+<div class="sidenav">
+	Help Desk:
+	<ul>
+		<li><a href="">Bank Accounts</a></li>
+		<li><a href="">Claims</a></li>
+		<li><a href="">Hearings</a></li>
+		<li><a href="">Tips for Beneficiaries</a></li>
+		<li><a href="">Tips for Fiduciaries</a></li>
+		<li><a href="">Tips for Filing Your Account</a></li>
+	</ul>
+	<br />
+	Contact Information:
+</div>
