eef348708f3c79f50a990d6f2a44473756a2e76e
Chris Rand <crand4@radford.edu>
2019-04-09 22:09:01 -0400

    updated sidebar to include stub pages

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,14 @@
 <div id="sidebar">
-	Help Desk:
-	<ul>
-		<li><a href="">Bank Accounts</a></li>
-		<li><a href="">Claims</a></li>
-		<li><a href="">Hearings</a></li>
-		<li><a href="">Tips for Beneficiaries</a></li>
-		<li><a href="">Tips for Fiduciaries</a></li>
-		<li><a href="">Tips for Filing Your Account</a></li>
-		<li><a href="">FAQ</a></li>
-	</ul>
-	<br />
-	Contact Information:
+    Help Desk:
+    <ul>
+        <li><a href="bankAccounts.php">Bank Accounts</a></li>
+        <li><a href="claims.php">Claims</a></li>
+        <li><a href="hearings.php">Hearings</a></li>
+        <li><a href="beneficiaryTips.php">Tips for Beneficiaries</a></li>
+        <li><a href="fiduciaryTips.php">Tips for Fiduciaries</a></li>
+        <li><a href="accountFilingTips.php">Tips for Filing Your Account</a></li>
+        <li><a href="">FAQ</a></li>
+    </ul>
+    <br />
+    Contact Information:
 </div>
