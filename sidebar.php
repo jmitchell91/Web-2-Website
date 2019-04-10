cba3be8e309f8aad50c5e8029c1cd1e6f2e52fa6
jmitchell91 <jmitchell91@email.radford.edu>
2019-04-09 22:14:24 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <div id="sidebar">
+<<<<<<< HEAD
 	Help Desk:
 	<ul>
 		<li><a href="">Bank Accounts</a></li>
@@ -11,4 +12,18 @@
 	</ul>
 	<br />
 	Contact Information:
+=======
+    Help Desk:
+    <ul>
+        <li><a href="bankAccounts.php">Bank Accounts</a></li>
+        <li><a href="claims.php">Claims</a></li>
+        <li><a href="hearings.php">Hearings</a></li>
+        <li><a href="beneficiaryTips.php">Tips for Beneficiaries</a></li>
+        <li><a href="fiduciaryTips.php">Tips for Fiduciaries</a></li>
+        <li><a href="accountFilingTips.php">Tips for Filing Your Account</a></li>
+        <li><a href="">FAQ</a></li>
+    </ul>
+    <br />
+    Contact Information:
+>>>>>>> eef348708f3c79f50a990d6f2a44473756a2e76e
 </div>
